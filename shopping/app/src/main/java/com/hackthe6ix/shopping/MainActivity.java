da1cdc4d8828364e053871142394499d94f21974
Kamran Tayyab <kam@Kamrant.com>
2018-08-25 22:19:11 -0400

    Drawing

--- shopping/app/src/main/java/com/hackthe6ix/shopping/MainActivity.java
+++ shopping/app/src/main/java/com/hackthe6ix/shopping/MainActivity.java
@@ -1,41 +1,19 @@
 package com.hackthe6ix.shopping;
 
-import android.app.Activity;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.VectorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
-import android.widget.ImageView;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends Activity {
-    Draw draw;
-    ImageView myImg;
+public class MainActivity extends AppCompatActivity {
+    ViewGroup group;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        draw = new Draw(this);
-
-        draw.setBackgroundColor(Color.TRANSPARENT);
-        setContentView(draw);
-        loadImage();
-
-
+        group = (ViewGroup) getWindow().getDecorView();
+        Draw draw = new Draw(this);
+        group.addView(draw);
 
     }
-
-    void loadImage(){
-        Resources res = getResources();
-        Drawable drawable = res.getDrawable(R.drawable.ic_map);
-        VectorDrawable vectorDrawable =  (VectorDrawable) drawable;
-    }
-
 }
